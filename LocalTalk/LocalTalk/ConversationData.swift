794137f9c6244a2b726b38a3d582732ea909444e
Brian Douglas <ilikerobot@gmail.com>
2015-09-12 06:23:55 -0700

    attempt to add conversations

---
+++ LocalTalk/LocalTalk/ConversationData.swift
@@ -0,0 +1,15 @@
+//
+//  ConversationData.swift
+//  LocalTalk
+//
+//  Created by Brian Douglas on 9/11/15.
+//  Copyright (c) 2015 MutualFun. All rights reserved.
+//
+
+import UIKit
+
+class ConversationData: NSObject, UICollectionViewDataSource {
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        20
+    }
+}
