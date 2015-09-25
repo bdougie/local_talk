dd903d2eea338a941962e912815917bc440c295f
Brian Douglas <ilikerobot@gmail.com>
2015-09-25 07:19:18 -0700

    adds mpcviewcontroller

--- LocalTalk/LocalTalk/ConversationViewController.swift
+++ LocalTalk/LocalTalk/ConversationViewController.swift
@@ -7,20 +7,91 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
-class ConversationViewController: UIViewController {
-    @IBOutlet weak var Conversations : ConversationData?
-//    @IBOutlet weak var addConversation: UIBarButtonItem!
+class ConversationViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessionDelegate {
+    let serviceType = "LCOC-Chat"
     
+    var browser : MCBrowserViewController!
+    var assistant : MCAdvertiserAssistant!
+    var session : MCSession!
+    var peerID: MCPeerID!
+    
+    @IBOutlet weak var Conversations : ConversationData?
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // create the browser viewcontroller with a unique service name
+        self.browser = MCBrowserViewController(serviceType:serviceType,
+            session:self.session)
+        
+        self.browser.delegate = self;
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func addConversation(sender: UIBarButtonItem) {
+        print("button pressed ")
+        self.presentViewController(self.browser, animated: true, completion: nil)
     }
+    
+     // MARK: MCBrowserViewControllerDelegate method implementation
+    
+    func browserViewControllerDidFinish(
+        browserViewController: MCBrowserViewController)  {
+            // Called when the browser view controller is dismissed (ie the Done
+            // button was tapped)
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func browserViewControllerWasCancelled(
+        browserViewController: MCBrowserViewController)  {
+            // Called when the browser view controller is cancelled
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // MARK: MCBrowserSessionDelegate method implementation
+    
+    func session(session: MCSession, didReceiveData data: NSData,
+        fromPeer peerID: MCPeerID)  {
+            // Called when a peer sends an NSData to us
+            
+            // This needs to run on the main queue
+//            dispatch_async(dispatch_get_main_queue()) {
+//                
+//                var msg = NSString(data: data, encoding: NSUTF8StringEncoding)
+//                
+//                self.updateChat(msg, fromPeer: peerID)
+//            }
+    }
+    
+    // The following methods do nothing, but the MCSessionDelegate protocol
+    // requires that we implement them.
+    func session(session: MCSession,
+        didStartReceivingResourceWithName resourceName: String,
+        fromPeer peerID: MCPeerID, withProgress progress: NSProgress)  {
+            
+            // Called when a peer starts sending a file to us
+    }
+    
+    func session(session: MCSession,
+        didFinishReceivingResourceWithName resourceName: String,
+        fromPeer peerID: MCPeerID,
+        atURL localURL: NSURL, withError error: NSError?)  {
+            // Called when a file has finished transferring from another peer
+    }
+    
+    func session(session: MCSession, didReceiveStream stream: NSInputStream,
+        withName streamName: String, fromPeer peerID: MCPeerID)  {
+            // Called when a peer establishes a stream with us
+    }
+    
+    func session(session: MCSession, peer peerID: MCPeerID,
+        didChangeState state: MCSessionState)  {
+            // Called when a connected peer changes state (for example, goes offline)
+            
+    }
+
 
 }
 
