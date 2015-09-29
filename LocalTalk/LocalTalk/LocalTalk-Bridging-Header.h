a0e9b795a67207bb45cd46c993bdef339902c1f3
rickyPanzer <richardrpanzer@gmail.com>
2015-09-29 13:19:27 -0400

    configured bridging header

---
+++ LocalTalk/LocalTalk/LocalTalk-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  LocalTalk-Bridging-Header.h
+//  LocalTalk
+//
+//  Created by Brian Douglas on 9/20/15.
+//  Copyright (c) 2015 MutualFun. All rights reserved.
+//
+
+#import <JSQMessagesViewController/JSQMessage.h>
